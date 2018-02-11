ed6e8233528cfed64cfc23584b29621491f0741d
Isterikus <kovalenko27071996@gmail.com>
2018-02-11 17:17:47 +0200

    expanding...

--- Python/expand.py
+++ Python/expand.py
@@ -3,6 +3,7 @@ c = 5
 min_i = 1
 max_c = 6
 min_slices = [{'i': 2, 'j': 1}, {'i': 1, 'j': 2}]
+max_slices = [{'i': 6, 'j': 1}, {'i': 3, 'j': 2}, {'i': 2, 'j': 3}, {'i': 1, 'j': 6}]
 pizza = [[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0]]
 pizza_sliced = [[0, 1, 2, 3, 0], [0, 1, 2, 3, 0], [0, 0, 0, 0, 0]]
 expd = []
@@ -17,12 +18,9 @@ def get_size(i, j):
 		k += 1
 	return {'i': l, 'j': k}
 
-def get_max_exp(i, j, pie):
-
-
 for i in range(r):
 	for j in range(c):
 		val = pizza_sliced[i][j]
-		if val:
+		if val and val not in expd:
 			expd.append(val)
-		pie = get_size(i, j)
+			pie = get_size(i, j)
